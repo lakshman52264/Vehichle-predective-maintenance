8922cfccacc6ee773e8d7b904222e92dd7439106
lakshman52264 <144992465+lakshman52264@users.noreply.github.com>
2024-11-17 14:23:57 +0530

    Update firebaseConfig.js

--- react/firebaseConfig.js
+++ react/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmJnSxI_AfP7LXN8gFn_3VyD6ONu7jRuk",
-    authDomain: "predictive-maintenance-f112a.firebaseapp.com",
-    projectId: "predictive-maintenance-f112a",
-    storageBucket: "predictive-maintenance-f112a.appspot.com", // Corrected storage bucket URL
-    messagingSenderId: "741287481031",
-    appId: "1:741287481031:web:fca1881b805013085278e2",
-    measurementId: "G-4SHNTK97FZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "", // Corrected storage bucket URL
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase app
